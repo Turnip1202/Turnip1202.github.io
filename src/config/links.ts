84cf44e1e61d195d38af982595e2a9ea1526ada1
Turnip1202 <turnip1202@outlook.com>
2025-09-23 19:05:20 +0800

    change

--- src/config/links.ts
+++ src/config/links.ts
@@ -46,7 +46,8 @@ export const linkCategories: LinkCategory[] = [
       { id: 3, name: "稀土掘金", url: "https://juejin.cn/user/1684912023022440", icon: "🔨" },         // 替代：使用工具图标
       { id: 4, name: "哔哩哔哩", url: "https://b23.tv/zpySzz9", icon: "🎮" },      // 替代：使用游戏图标
       { id: 5, name: "抖音", url: "https://v.douyin.com/if78aSq9/", icon: "🎬" } ,         // 替代：使用电影图标
-      { id: 6, name: "react-study", url: "https://turnip1202.github.io/react-study", icon: "📋" }          // 替代：使用电影图标
+      { id: 6, name: "react-study", url: "https://turnip1202.github.io/react-study", icon: "📋" },          // 替代：使用电影图标
+      { id: 7, name: "ToDo-List", url: "https://turnip1202.github.io/react18-todo", icon: "📝" }          // 替代：使用电影图标
     ]
   },
   {
